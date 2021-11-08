be708f7afd9de1e8cf961a7d593fd325b331f5ee
b00132407 <91259835+b00132407@users.noreply.github.com>
2021-11-08 14:44:17 +0000

    PowerUp
    Power up is spawned

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     public GameObject[] zombiePrefabs;
+    public GameObject powerupPrefab;
     private float spawnRangeX = -15;
     private float spawnPosZ = -5;
+    private float spawnPosY = 5;
     private float startDelay = 2;
     private float spawnInterval = 2.5f;
 
@@ -20,15 +22,33 @@ public class SpawnEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SpawnPowerUp();
 
+    }
+
+    Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, spawnPosY, zPos);
     }
 
     void SpawnZombies()
     {
-        //Randomly generate zombie index and spawn location
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         int zombieIndex = Random.Range(0, zombiePrefabs.Length);
 
-        Instantiate(zombiePrefabs[zombieIndex], spawnPos, zombiePrefabs[zombieIndex].transform.rotation);
+        Instantiate(zombiePrefabs[zombieIndex], GenerateSpawnPosition(), zombiePrefabs[zombieIndex].transform.rotation);
+    }
+
+    void SpawnPowerUp()
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
     }
 }
