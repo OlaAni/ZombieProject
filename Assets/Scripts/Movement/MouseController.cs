f872e2d8d7c74143fe52e58e9116924d485950e8
OlaAni <b00134084@mytudublin.ie>
2021-11-29 13:36:04 +0000

    Fixed animations
    Need to lower y position of both ammo pickups

--- Assets/Scripts/Movement/MouseController.cs
+++ Assets/Scripts/Movement/MouseController.cs
@@ -102,13 +102,5 @@ public class MouseController : MonoBehaviour
                 }
             }
         }
-
-
-        if (Input.GetKey(KeyCode.E)) 
-        {
-        
-        }
-
-
     }
 }
