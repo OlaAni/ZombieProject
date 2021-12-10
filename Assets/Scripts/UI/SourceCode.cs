7e68a8b6a5adb4ebb40b75571ca8ef3a2568d1e6
b00132407 <91259835+b00132407@users.noreply.github.com>
2021-12-10 16:32:34 +0000

    Source button

---
+++ Assets/Scripts/UI/SourceCode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceCode : MonoBehaviour
+{
+    public void openWebsite()
+    {
+        Application.OpenUrl("https://github.com/OlaAni/ZombieProject/tree/Beta/Assets");
+
+    }
+}
